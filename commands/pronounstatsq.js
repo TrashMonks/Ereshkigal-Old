915943608cc8067bf254a9d28c2946e89b95c208
unormal <support@freeholdgames.com>
2021-10-03 18:40:11 +0000

    Add untracked files from Caves of Qud
    These files were present on the Caves of Qud discord bot Ereshkigal
    but were not being tracked in Git. Applying here to show fork relationship
    between the two repositories.

--- commands/pronounstatsq.js
+++ commands/pronounstatsq.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'pronounstatsqud',
+  name: 'pronounstatsq',
   description: 'Shows the stats for how many people are using pronoun roles on your server',
   usage: '',
   cooldown: 3,
@@ -12,14 +12,15 @@ module.exports = {
       return Math.round(((number * 100) + Number.EPSILON) * 100) / 100;
     }
 
-    let content = '__**Pronoun Role Usage (airlock):**__\n';
+    let content = '__**Pronoun Role Usage (Delver only):**__\n';
     const fullMemberList = await message.guild.members.fetch();
     // Detect pronoun roles by searching for ones with a slash in them. Might update this later to let people exclude certain roles ig? This is fine for our purposes right now
-    const pronounRoles = await message.guild.roles.cache.filter(role => (!role.name.includes('Delver')) && (role.name.includes('/') || role.name.toLowerCase().includes('pronoun'))).sort((a, b) => a.name.localeCompare(b.name));
+    const pronounRoles = await message.guild.roles.cache.filter(role => role.name.includes('/') || role.name.toLowerCase().includes('pronoun')).sort((a, b) => a.name.localeCompare(b.name));
     // Get total # of members with and without at least one pronoun role
-    const membersWithPronounRoles = await message.guild.members.cache.filter(u => u.roles.cache.find(r => pronounRoles.find(pronounRole => pronounRole === r)) && !u.user.bot).size;
-    const membersWithoutPronounRoles = await message.guild.members.cache.filter(u => !u.roles.cache.find(r => pronounRoles.find(pronounRole => pronounRole === r)) && !u.user.bot).size;
+    const membersWithPronounRoles = await message.guild.members.cache.filter(u => u.roles.cache.find(r => pronounRoles.find(pronounRole => pronounRole === r )) && u.roles.cache.find(r => r.name === 'Delver') && !u.user.bot).size;
+    const membersWithoutPronounRoles = await message.guild.members.cache.filter(u => !u.roles.cache.find(r => pronounRoles.find(pronounRole => pronounRole === r )) && u.roles.cache.find(r => r.name === 'Delver') && !u.user.bot).size;
     const totalRolesApplied = pronounRoles.reduce((prevVal, role) => prevVal + role.members.size, 0);
+    const selectedMembers = await message.guild.members.cache.filter(u => u.roles.cache.find(r => u.roles.cache.find(r => r.name === 'Delver') && !u.user.bot)).size;
 
     // Iterate through each pronoun role to get the stats for it
     for (curRole of pronounRoles) {
@@ -27,14 +28,15 @@ module.exports = {
       // Create a new collection without the current role (for exclusive sizing)
       const otherRoles = pronounRoles.filter(r => r.name !== curRole.name);
       // Get the number of members with this pronoun role and no others
-      const exclusiveSize = curRole.members.filter(user => !user.roles.cache.some(r => otherRoles.find(otherRole => otherRole === r))).size;
+      const curRoleSize = curRole.members.filter(user => user.roles.cache.find(r => r.name === 'Delver')).size;
+      const exclusiveSize = curRole.members.filter(user => !user.roles.cache.some(r => otherRoles.find(otherRole => otherRole === r)) && user.roles.cache.find(r => r.name === 'Delver')).size;
       // Make sure we avoid any roles with pronoun in them that have 0 members
       if (curRole.members.size > 0) {
       // Assign each value to an object to retrieve below, so that we can print them once totalRolesApplied is done calculating
         const members = (curRole.members.size == 1) ? 'member' : 'members';
         const exclMembers = (exclusiveSize == 1) ? 'member' : 'members';
         content +=
-      `> **${curRole.name}**: ${curRole.members.size} ${members} (${round(curRole.members.size / totalRolesApplied)}%)
+      `> **${curRole.name}**: ${curRoleSize} ${members} (${round(curRoleSize / membersWithPronounRoles)}%)
       > - Exclusive: ${exclusiveSize} ${exclMembers} (${round(exclusiveSize / membersWithPronounRoles)}%)\n\n`;
       }
     }
@@ -42,10 +44,9 @@ module.exports = {
     // More stats
     content += '__**Server Pronoun Role Stats:**__\n';
     content +=
-  `> Total Applied: ${totalRolesApplied}
-  > Members with: ${membersWithPronounRoles}
+  `> Members with: ${membersWithPronounRoles}
   > Members without: ${membersWithoutPronounRoles}
-  > Total Server Members: ${fullMemberList.size}`;
+  > Total Members: ${selectedMembers}`;
 
     // And, send
     message.channel.send(content);
